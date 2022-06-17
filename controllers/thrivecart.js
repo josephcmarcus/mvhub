51ff2c2b2eae1fa28fe6817aad7e601f2ac0fdf2
Joe Marcus <josephcmarcus@gmail.com>
2022-06-17 10:44:20 -0400

    Rework thrivecart controller to only accept sale hooks

--- controllers/thrivecart.js
+++ controllers/thrivecart.js
@@ -23,10 +23,7 @@ module.exports.sale = (req, res) => {
 };
 
 module.exports.processSaleHook = (req, res) => {
-    const { event, thrivecart_account, thrivecart_secret, order_id, invoice_id, order_date, 
-        order_timestamp, currency, customer_id, customer: { email, address: { country, state, 
-        line1: street, city, zip }, ip_address, first_name, last_name, checkbox_confirmation }, 
-        order: { processor, charges }} = req.body;
+    const { event, thrivecart_secret } = req.body;
     console.log(`***** Start POST at /thrivecart/sale *****`);
     console.log(req.body);
     console.log(`***** End POST at /thrivecart/sale *****`);
@@ -34,8 +31,14 @@ module.exports.processSaleHook = (req, res) => {
     if (thrivecart_secret != process.env.TC_SECRET) {
         console.log('Unauthorized')
         res.status(401).send('Unauthorized');
+    } else if (event != 'order.success') {
+        console.log('Unauthorized - Not a Sale')
+        res.status(401).send('Unauthorized - Not a Sale');
     } else {
-        console.log('Authorized')
+        const { thrivecart_account, order_id, invoice_id, order_date, order_timestamp, currency, 
+            customer_id, customer: { email, address: { country, state, line1: street, city, zip }, 
+            ip_address, first_name, last_name, checkbox_confirmation }, order: { processor, charges }} = req.body;
+        console.log('Authorized - Sale')
         for (let i = 0; i < charges.length; i++) {
             const columns = `event, thrivecart_account, thrivecart_secret, order_id, invoice_id, order_date,` + 
             `order_timestamp, currency, customer_id, first_name, last_name, email, country, state, street, city,` + 
