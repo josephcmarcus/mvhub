181e8efaa676be05f2f0bc1565aed18f0d0a3fc3
Joe Marcus <josephcmarcus@gmail.com>
2022-10-11 23:36:14 -0400

    Add basic auth to microservice handler

--- utils/microservicehandler.js
+++ utils/microservicehandler.js
@@ -1,9 +1,10 @@
+const dotenv = require('dotenv').config();
 const axios = require('axios');
 
 const mailchimp = {
   findMember: async function(email) {
     try {
-      const response = await axios.get(`http://localhost:3001/findmember/lists/e10aaf22f0/members/${email}`)
+      const response = await axios.get(`https://mv-hub-micro.azurewebsites.net/findmember/${process.env.MICRO_API_KEY}/lists/${process.env.SCHOOL_ID}/members/${email}`)
       console.log(response);
     } catch(error) {
       console.log(error);
@@ -14,7 +15,7 @@ const mailchimp = {
     if (product === 'Demolishing Demonic Assignments Backstage Pass') {
       const tagName = 'INT VIP Demolishing Demonic Assignments';
       try {
-        const response = await axios.post(`http://localhost:3001/addtag/lists/e10aaf22f0/members/${email}/${tagName}/active`)
+        const response = await axios.post(`https://mv-hub-micro.azurewebsites.net/addtag/${process.env.MICRO_API_KEY}/lists/${process.env.SCHOOL_ID}/members/${email}/${tagName}/active`)
         console.log(response);
       } catch(error) {
         console.log(error);
