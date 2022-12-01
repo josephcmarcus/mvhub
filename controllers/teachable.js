4d95d25bf7304462f01762a83d4677c0e8f91745
Joe Marcus <josephcmarcus@gmail.com>
2022-12-01 12:55:52 -0500

    Refine teachable controller newObj destructuring

--- controllers/teachable.js
+++ controllers/teachable.js
@@ -11,14 +11,14 @@ module.exports.processSaleHook = (req, res) => {
     console.log(`***** End POST at /teachable/sale *****`);
 
     const newObj = req.body.shift(); // pulls webhook out of array into standalone object
-    const { type: event_type, object: { user: { name: email}, course: { name: coursename }}} = newObj;
+    const { type: event_type, object: { user: { email }, course: { name }}} = newObj;
 
         if (event_type != 'Sale.created') {
             res.status(401).send('Unauthorized - Not a Sale');
         };
 
         // passes course name and email to the Mailchimp add tag handler
-        mailchimp.addTag(coursename, email);
+        mailchimp.addTag(name, email);
 
     res.status(200).send('Success');
 };
